bbdcd0190f0908a7a60bd01cea0f72b7acd86e1b
mainul96 <110577655+mainul96@users.noreply.github.com>
2024-07-20 22:53:05 +0530

    zod validation

---
+++ src/app/errors/handleZodError.ts
@@ -0,0 +1,19 @@
+import { ZodError, ZodIssue } from 'zod';
+import { TErrorSource } from '../middlewares/globalErrorHandler';
+
+const handleZodError = (err: ZodError) => {
+  const errorSources: TErrorSource = err.issues.map((issue: ZodIssue) => {
+    return {
+      path: issue?.path[issue.path.length - 1],
+      message: issue.message,
+    };
+  });
+  const statusCode = 400;
+  return {
+    statusCode,
+    message: 'Validation error',
+    errorSources,
+  };
+};
+
+export default handleZodError;
--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,17 +1,41 @@
-import e, { NextFunction, Response, Request } from 'express';
+import { NextFunction, Response, Request, ErrorRequestHandler } from 'express';
+import { ZodError, ZodIssue } from 'zod';
+import config from '../config';
+import handleZodError from '../errors/handleZodError';
 
-const globalErrorHandler = (
-  err: any,
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  const statusCode = err.statusCode || 500;
-  const message = err.message || 'something went is wrong!';
-  return res.status(statusCode).json({
-    success: false,
+export type TErrorSource = {
+  path: string | number;
+  message: string;
+}[];
+
+const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  let statusCode =  500;
+ let message ='something went is wrong!';
+
+  let errorSources: TErrorSource = [
+    {
+      path: '',
+      message: 'something went is wrong!',
+    },
+  ];
+
+  // const statusCode = 400;
+
+  
+
+  if (err instanceof ZodError) {
+    const simplifiedError = handleZodError(err);
+   statusCode = simplifiedError?.statusCode;
+   message = simplifiedError?.message;
+   errorSources = simplifiedError?.errorSources.
+  }
+
+  return res.status(statusCode).json( {
+    success:false,
     message,
-    error: err,
-  });
+    errorSources,
+    err,
+    stack: config.NODE_ENV === 'development' ? err?.stack : null
+  })
 };
 export default globalErrorHandler;
